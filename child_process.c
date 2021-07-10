143a1a767ef337309c346fbfe6d1fd18b59aafa9
Denis Korchagin <denis.korchagin.1995@gmail.com>
2021-07-10 21:58:50 +0300

    shut up the non captured child process output

--- child_process.c
+++ child_process.c
@@ -5,14 +5,20 @@
 
 #include "headers/child_process.h"
 
+#define PIPE_COUNT (2)
+
+#define PIPE_STDOUT (0)
+#define PIPE_STDERR (1)
+
 #define PIPE_INPUT_FD   (0)
 #define PIPE_OUTPUT_FD  (1)
 
-static int fd[2] = {0};
+static int pipes[PIPE_COUNT][2];
 
 void child_process_run(const char * path, char * argv[], struct process_output * output, unsigned int mode)
 {
-    pipe(fd);
+    pipe(pipes[PIPE_STDOUT]);
+    pipe(pipes[PIPE_STDERR]);
 
     pid_t pid = fork();
 
@@ -21,32 +27,35 @@ void child_process_run(const char * path, char * argv[], struct process_output *
         exit(1);
     }
 
-    int replace_fd = -1;
+    int pipe_index = -1;
 
     switch (mode) {
         case RUN_MODE_CAPTURE_STDOUT:
-            replace_fd = STDOUT_FILENO;
+            pipe_index = PIPE_STDOUT;
             break;
         case RUN_MODE_CAPTURE_STDERR:
-            replace_fd = STDERR_FILENO;
+            pipe_index = PIPE_STDERR;
             break;
     }
 
-    if (replace_fd == -1) {
+    if (pipe_index == -1) {
         fprintf(stderr, "unknown mode %u\n", mode);
         exit(1);
     }
 
     if (pid == 0) {
-        dup2(fd[PIPE_OUTPUT_FD], replace_fd);
+        dup2(pipes[PIPE_STDOUT][PIPE_OUTPUT_FD], STDOUT_FILENO);
+        dup2(pipes[PIPE_STDERR][PIPE_OUTPUT_FD], STDERR_FILENO);
 
-        close(fd[PIPE_OUTPUT_FD]);
-        close(fd[PIPE_INPUT_FD]);
+        close(pipes[PIPE_STDOUT][PIPE_OUTPUT_FD]);
+        close(pipes[PIPE_STDOUT][PIPE_INPUT_FD]);
+        close(pipes[PIPE_STDERR][PIPE_OUTPUT_FD]);
+        close(pipes[PIPE_STDERR][PIPE_INPUT_FD]);
 
         int status = execv(path, argv);
 
         if (status != 0) {
-            fprintf(stderr, "exec error\n");
+            fprintf(stderr, "exec error: %d\n", status);
             exit(1);
         }
 
@@ -55,9 +64,10 @@ void child_process_run(const char * path, char * argv[], struct process_output *
 
     wait(NULL);
 
-    close(fd[PIPE_OUTPUT_FD]);
+    close(pipes[PIPE_STDOUT][PIPE_OUTPUT_FD]);
+    close(pipes[PIPE_STDERR][PIPE_OUTPUT_FD]);
 
-    ssize_t nbytes = read(fd[PIPE_INPUT_FD], output->buffer, output->size);
+    ssize_t nbytes = read(pipes[pipe_index][PIPE_INPUT_FD], output->buffer, output->size);
 
     if (nbytes < 0) {
         fprintf(stderr, "IO error\n");
@@ -65,5 +75,8 @@ void child_process_run(const char * path, char * argv[], struct process_output *
     }
 
     output->len = (unsigned int)nbytes;
+
+    close(pipes[PIPE_STDOUT][PIPE_INPUT_FD]);
+    close(pipes[PIPE_STDERR][PIPE_INPUT_FD]);
 }
 
